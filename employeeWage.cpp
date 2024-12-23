8eec0d8ff2e17953f5e7ba5946a6d96ad76392b7
shivamrajput18 <shivamsingh80094@gmail.com>
2024-12-23 14:55:21 +0530

    Add : employeeAttendance

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void employeeAttendance(){
+   srand(time(0));
+      int randomNum = rand() % 2;
+
+    if(randomNum==1){
+         cout << "Employee is Present" << endl;
+    }
+    else{
+          cout << "Employee is Absent" << endl;
+    }
+}
+
 int main(){
      cout << "Welcome to employee-wage-computation" << endl;
+
+     employeeAttendance();
 
      return 0;
 }

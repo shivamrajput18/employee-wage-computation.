5377dfe545c7cf3dcdf288960ae373567ba9b82e
shivamrajput18 <shivamsingh80094@gmail.com>
2024-12-24 10:41:49 +0530

    Calculate : totalWages for a condition

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -4,46 +4,72 @@
 
 using namespace std;
 
-int calculateDailyWage(int wagePerHour, int dayHours)
-{
+int calculateDailyWage(int wagePerHour, int dayHours) {
     return wagePerHour * dayHours;
 }
 
-void employeeAttendance()
-{
-    srand(time(0));
-    int wagePerHour = 20;
-    int totalWage = 0;
+void employeeAttendance() {
+    srand(time(0));  
+    int wagePerHour = 20;  
+    int totalWage = 0;     
+    int totalHoursWorked = 0;  
+    int totalDaysWorked = 0;   
+    int maxHours = 100;        
+    int maxDays = 20;         
 
-    for (int i = 1; i <= 20; i++)
-    {
-        int randomNum = rand() % 2;
+    while (totalHoursWorked < maxHours && totalDaysWorked < maxDays) {
+        int randomNum = rand() % 2; 
+        int dayHours = (rand() % 2 == 0) ? 4 : 8; 
 
-        switch (randomNum)
-        {
-          case 1:
-          {
-            int dayHours = (rand() % 2 == 0) ? 4 : 8;
-            int dailyWage = calculateDailyWage(wagePerHour, dayHours);
-            totalWage += dailyWage;
-            cout << "Day " << i << ": Employee is Present, Worked Hours: " << dayHours
-                 << ", Daily Wage: " << dailyWage << endl;
-            break;
-          }
+        switch (randomNum) {
+            case 1: {  
+                switch (dayHours) {
+                    case 8: {  
+                        int dailyWage = calculateDailyWage(wagePerHour, dayHours);
+                        totalWage += dailyWage;
+                        totalHoursWorked += dayHours;
+                        totalDaysWorked++;
+                        cout << "Day " << totalDaysWorked << ": Employee is Present, Worked Hours: " 
+                             << dayHours << ", Daily Wage: " << dailyWage 
+                             << ", Total Hours Worked: " << totalHoursWorked << endl;
+                        break;
+                    }
 
-          case 0:
-          {
-            cout << "Day " << i << ": Employee is Absent" << endl;
-            break;
-          }
+                    case 4: {  
+                        int dailyWage = calculateDailyWage(wagePerHour, dayHours);
+                        totalWage += dailyWage;
+                        totalHoursWorked += dayHours;
+                        totalDaysWorked++;
+                        cout << "Day " << totalDaysWorked << ": Employee is Present, Worked Hours: " 
+                             << dayHours << ", Daily Wage: " << dailyWage 
+                             << ", Total Hours Worked: " << totalHoursWorked << endl;
+                        break;
+                    }
+
+                    default:
+                        cout << "Invalid input for working hours!" << endl;
+                        break;
+                }
+                break;
+            }
+
+            case 0: {  
+                cout << "Day " << totalDaysWorked + 1 << ": Employee is Absent" << endl;
+                totalDaysWorked++;
+                break;
+            }
+
+            default:
+                cout << "Unexpected error!" << endl;
+                break;
         }
     }
 
-    cout << "\nTotal Wage for 20 Days: " << totalWage << endl;
+
+    cout << "\nTotal Wage after reaching condition (100 hours or 20 days): " << totalWage << endl;
 }
 
-int main()
-{
+int main() {
     cout << "Welcome to Employee Wage Computation" << endl;
 
     employeeAttendance();

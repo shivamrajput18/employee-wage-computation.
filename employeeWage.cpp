7c73766b9111731453b2f3ef213fd35aa232cf6e
shivamrajput18 <shivamsingh80094@gmail.com>
2024-12-24 10:52:52 +0530

    Solving : using class and method

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -4,75 +4,63 @@
 
 using namespace std;
 
-int calculateDailyWage(int wagePerHour, int dayHours) {
-    return wagePerHour * dayHours;
-}
+class EmployeeWage {
+private:
+    int wagePerHour;        
+    int totalWage;            
+    int totalHoursWorked;     
+    int totalDaysWorked;    
+    const int maxHours = 100; 
+    const int maxDays = 20;   
 
-void employeeAttendance() {
-    srand(time(0));  
-    int wagePerHour = 20;  
-    int totalWage = 0;     
-    int totalHoursWorked = 0;  
-    int totalDaysWorked = 0;   
-    int maxHours = 100;        
-    int maxDays = 20;         
-
-    while (totalHoursWorked < maxHours && totalDaysWorked < maxDays) {
-        int randomNum = rand() % 2; 
-        int dayHours = (rand() % 2 == 0) ? 4 : 8; 
-
-        switch (randomNum) {
-            case 1: {  
-                switch (dayHours) {
-                    case 8: {  
-                        int dailyWage = calculateDailyWage(wagePerHour, dayHours);
-                        totalWage += dailyWage;
-                        totalHoursWorked += dayHours;
-                        totalDaysWorked++;
-                        cout << "Day " << totalDaysWorked << ": Employee is Present, Worked Hours: " 
-                             << dayHours << ", Daily Wage: " << dailyWage 
-                             << ", Total Hours Worked: " << totalHoursWorked << endl;
-                        break;
-                    }
-
-                    case 4: {  
-                        int dailyWage = calculateDailyWage(wagePerHour, dayHours);
-                        totalWage += dailyWage;
-                        totalHoursWorked += dayHours;
-                        totalDaysWorked++;
-                        cout << "Day " << totalDaysWorked << ": Employee is Present, Worked Hours: " 
-                             << dayHours << ", Daily Wage: " << dailyWage 
-                             << ", Total Hours Worked: " << totalHoursWorked << endl;
-                        break;
-                    }
-
-                    default:
-                        cout << "Invalid input for working hours!" << endl;
-                        break;
-                }
-                break;
-            }
-
-            case 0: {  
-                cout << "Day " << totalDaysWorked + 1 << ": Employee is Absent" << endl;
-                totalDaysWorked++;
-                break;
-            }
-
-            default:
-                cout << "Unexpected error!" << endl;
-                break;
-        }
+public:
+    EmployeeWage(int wagePerHour) {
+        this->wagePerHour = wagePerHour;
+        totalWage = 0;
+        totalHoursWorked = 0;
+        totalDaysWorked = 0;
     }
 
+    int calculateDailyWage(int dayHours) {
+        return wagePerHour * dayHours;
+    }
 
-    cout << "\nTotal Wage after reaching condition (100 hours or 20 days): " << totalWage << endl;
-}
+    void calculateWageForMonth() {
+        srand(time(0)); 
+
+        while (totalHoursWorked < maxHours && totalDaysWorked < maxDays) {
+            int randomNum = rand() % 2; 
+            int dayHours = (rand() % 2 == 0) ? 4 : 8; 
+
+            switch (randomNum) {
+                case 1: {  
+                    int dailyWage = calculateDailyWage(dayHours);
+                    totalWage += dailyWage;
+                    totalHoursWorked += dayHours;
+                    totalDaysWorked++;
+                    cout << "Day " << totalDaysWorked << ": Employee is Present, Worked Hours: "
+                         << dayHours << ", Daily Wage: " << dailyWage 
+                         << ", Total Hours Worked: " << totalHoursWorked << endl;
+                    break;
+                }
+
+                case 0: { 
+                    totalDaysWorked++;
+                    cout << "Day " << totalDaysWorked << ": Employee is Absent" << endl;
+                    break;
+                }
+
+            }
+        }
+
+        cout << "\nTotal Wage after reaching condition (100 hours or 20 days): " << totalWage << endl << endl;
+    }
+};
 
 int main() {
-    cout << "Welcome to Employee Wage Computation" << endl;
-
-    employeeAttendance();
+    cout << "Welcome to Employee Wage Computation Program" << endl;
+    EmployeeWage employee(20);
+    employee.calculateWageForMonth();
 
     return 0;
 }
